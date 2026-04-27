40a1b8405729439653de03b21a93d5447b28b5a5
Sutton Wilterdink <swilterdink1010@floridapoly.edu>
2026-04-26 23:42:52 -0400

    chore: removed data tracking

--- access_database/cop-3710-youtube-analytics.accdb.src/tbldefs/channel.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [channel] (
-  [channel_id] AUTOINCREMENT CONSTRAINT [Index_6039CAA6_0EEB_4F14] PRIMARY KEY UNIQUE NOT NULL,
-  [channel_name] VARCHAR (50),
-  [channel_subs] LONG,
-  [channel_views] VARCHAR
-)
