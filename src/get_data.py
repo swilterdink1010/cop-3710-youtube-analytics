1896564aff42d12978e0f0c7b2fd392847a43d26
Sutton Wilterdink <swilterdink1010@floridapoly.edu>
2026-04-26 23:47:40 -0400

    chore: move source code to src dir

---
+++ src/get_data.py
@@ -0,0 +1,25 @@
+import pandas
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+
+
+FILE_PATH = "./USvideos.csv"
+
+
+def get_data_raw()->pandas.DataFrame:
+    print("Retrieving dataset...")
+    data = kagglehub.dataset_load(
+        KaggleDatasetAdapter.PANDAS,
+        "datasnaek/youtube-new",
+        FILE_PATH,
+    )
+    return data
+
+
+def main():
+    raw_data = get_data_raw()
+    print(raw_data)
+    
+    
+if __name__ == "__main__":
+    main()
